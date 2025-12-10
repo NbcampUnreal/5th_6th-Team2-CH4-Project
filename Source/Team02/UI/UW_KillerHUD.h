32f71e5a8ce6f4df1395e48cc8f4a4a28bc457ba
p00-h <149079533+p00-h@users.noreply.github.com>
2025-12-10 20:01:21 +0900

    Add 킬러 UI 추가

---
+++ Source/Team02/UI/UW_KillerHUD.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UW_KillerHUD.generated.h"
+
+class UT2CooldownComponent;
+
+/**
+ * 
+ */
+UCLASS()
+class TEAM02_API UUW_KillerHUD : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
